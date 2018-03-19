6262d03febe3a1d7ed8368ba34dc1ce2567106c0
YingdongLi <lyd941026@gmail.com>
2018-03-19 11:31:35 -0400

    Angular Component part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http'; 
 import { MaterialModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
