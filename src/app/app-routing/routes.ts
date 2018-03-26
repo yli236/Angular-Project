9936478b281860567ac77d4a09bf3870cc6ac1c6
YingdongLi <lyd941026@gmail.com>
2018-03-26 14:10:07 -0400

    Single Page Application Part2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from '../home/home.component';
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
+    {path: 'dishdetail/:id', component: DishdetailComponent},
     {path: 'contactus', component: ContactComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
